2d03a6a659a319de738a9f39c1db91485d9b9c52
CARRION LOAYZA SHARMELEE ODETTE <166635431+Sharmelee-carrion@users.noreply.github.com>
2026-05-11 19:39:00 -0500

    Create configuracion.php

---
+++ configuracion.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Archivo de configuración general
+ */
+
+define('NOMBRE_PROYECTO', 'Carroña de Sharmelee');
+define('VERSION', '1.0.0');
+define('FECHA_CREACION', '2026-05-11');
+define('ENTORNO', 'desarrollo');
+
+// Configuración de errores
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
